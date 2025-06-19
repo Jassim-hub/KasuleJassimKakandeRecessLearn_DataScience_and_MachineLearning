13851f78ced0e49fb097c2cf63a2cfb70ed65ff5
jassim-hub <jassimkasule@gmail.com>
2025-06-19 12:57:59 +0300

    analyzed data and plotted effect of categorical columns on churn rate using bar graphs

---
+++ churn-prediction-tf/Similar_to_churn_model.py
@@ -0,0 +1,73 @@
+# <!-- # Code Citations
+
+# ## License: unknown
+# https://github.com/GlenCrawford/australia_rain_tomorrow_binary_classification_prediction/blob/2aad1a119e8ec9ed192f7b86b9fcfad6cb0dbf7d/main.py
+
+# ```
+# ,
+
+# ```
+
+
+# ## License: unknown
+# https://github.com/adyachok/kofer-ds-artifacts/blob/387ae52082a6a338a8405dc2b136908d96b97485/models/mod-text-class/build/scripts/train_model.py
+
+# ```
+# ,
+
+# ```
+
+
+# ## License: unknown
+# https://github.com/GlenCrawford/australia_rain_tomorrow_binary_classification_prediction/blob/2aad1a119e8ec9ed192f7b86b9fcfad6cb0dbf7d/main.py
+
+# ```
+# ,
+
+# ```
+
+
+# ## License: unknown
+# https://github.com/adyachok/kofer-ds-artifacts/blob/387ae52082a6a338a8405dc2b136908d96b97485/models/mod-text-class/build/scripts/train_model.py
+
+# ```
+# ,
+
+# ```
+
+
+# ## License: unknown
+# https://github.com/GlenCrawford/australia_rain_tomorrow_binary_classification_prediction/blob/2aad1a119e8ec9ed192f7b86b9fcfad6cb0dbf7d/main.py
+
+# ```
+# ,
+#         tf
+# ```
+
+
+# ## License: unknown
+# https://github.com/adyachok/kofer-ds-artifacts/blob/387ae52082a6a338a8405dc2b136908d96b97485/models/mod-text-class/build/scripts/train_model.py
+
+# ```
+# ,
+#         tf
+# ```
+
+
+# ## License: unknown
+# https://github.com/GlenCrawford/australia_rain_tomorrow_binary_classification_prediction/blob/2aad1a119e8ec9ed192f7b86b9fcfad6cb0dbf7d/main.py
+
+# ```
+# ,
+#         tf.keras
+# ```
+
+
+# ## License: unknown
+# https://github.com/adyachok/kofer-ds-artifacts/blob/387ae52082a6a338a8405dc2b136908d96b97485/models/mod-text-class/build/scripts/train_model.py
+
+# ```
+# ,
+#         tf.keras
+# ```
+#  -->
